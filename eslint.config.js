ee757c54b56a75dc2b59abd21ff2b1334acbe190
Alexei-github <a.beraziuk.student.unsw@gmail.com>
2025-02-01 23:10:11 +1100

    implement commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config([
   {
-    ignores: ['/dist', '/node_modules'],
+    ignores: ['dist', 'node_modules'],
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
